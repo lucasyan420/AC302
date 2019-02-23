2c79308c326391158f9cc3d3d0b8dbae18538c1f
lucasyan420 <lucasyan2005@gmail.com>
2019-02-23 15:49:27 +0800

    first push

--- Lesson7/app.js
+++ Lesson7/app.js
@@ -19,27 +19,27 @@ function create(){
 
 	game.add.sprite(0,0, 'sky');
 
-	platform = game.add.physicsGroup();
-	platform.enableBody = true;
+	// platform = game.add.physicsGroup();
+	// platform.enableBody = true;
 
-	var ground = platform.create(0, 550, 'platform');
-	//mutliples the height & width of ground image by 2
-	ground.scale.setTo(2,2);
-	//set ground to immovable
-	ground.body.immovable = true;
+	// var ground = platform.create(0, 550, 'platform');
+	// //mutliples the height & width of ground image by 2
+	// ground.scale.setTo(2,2);
+	// //set ground to immovable
+	// ground.body.immovable = true;
 
-	var platform1 = platform.create(-100,250, 'platform');
-	platform1.body.immovable = true;
+	// var platform1 = platform.create(-100,250, 'platform');
+	// platform1.body.immovable = true;
 
-	var platform2 = platform.create(400, 400, 'platform');
-	platform2.body.immovable = true;
+	// var platform2 = platform.create(400, 400, 'platform');
+	// platform2.body.immovable = true;
 
-	var style = {font: "bold 32px Arial", fill:"#fff"};
-	scorelabel = game.add.text(300,560, "Score:", style);
-	scoretext = game.add.text(420,560, score, style);
+	// var style = {font: "bold 32px Arial", fill:"#fff"};
+	// scorelabel = game.add.text(300,560, "Score:", style);
+	// scoretext = game.add.text(420,560, score, style);
 
-	lifelabel = game.add.text(10,5, "Life:", style);
-	lifetext = game.add.text(120,5, life, style);
+	// lifelabel = game.add.text(10,5, "Life:", style);
+	// lifetext = game.add.text(120,5, life, style);
 
 }
 
