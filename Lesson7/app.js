30e6bc74127978d7f54761cd829453e206f3d180
lucasyan420 <lucasyan2005@gmail.com>
2019-02-23 15:54:42 +0800

    first push

--- Lesson7/app.js
+++ Lesson7/app.js
@@ -4,11 +4,11 @@ var score = 0;
 var life = 3;
 
 function preload(){
-	//static image
-	//sprite sheet (animations or different image)
-	game.load.image ('sky', 'sky.png');
+	// static image
+	// sprite sheet (animations or different image)
+	game.load.image('sky','sky.png');
 	// game.load.spritesheet("player", "assets/dude.png", 32, 48);
-	game.load.image ('platform', 'platform.png');
+	game.load.image('platform', 'platform.png');
 	// game.load.image ("star", "assets/star.png")
 	// game.load.spritesheet("baddie", "assets/baddie.png", 32, 32);
 
@@ -19,20 +19,20 @@ function create(){
 
 	game.add.sprite(0,0, 'sky');
 
-	// platform = game.add.physicsGroup();
-	// platform.enableBody = true;
+	platform = game.add.physicsGroup();
+	platform.enableBody = true;
 
-	// var ground = platform.create(0, 550, 'platform');
-	// //mutliples the height & width of ground image by 2
-	// ground.scale.setTo(2,2);
-	// //set ground to immovable
-	// ground.body.immovable = true;
+	var ground = platform.create(0, 550, 'platform');
+	//mutliples the height & width of ground image by 2
+	ground.scale.setTo(2,2);
+	//set ground to immovable
+	ground.body.immovable = true;
 
-	// var platform1 = platform.create(-100,250, 'platform');
-	// platform1.body.immovable = true;
+	var platform1 = platform.create(-100,250, 'platform');
+	platform1.body.immovable = true;
 
-	// var platform2 = platform.create(400, 400, 'platform');
-	// platform2.body.immovable = true;
+	var platform2 = platform.create(400, 400, 'platform');
+	platform2.body.immovable = true;
 
 	// var style = {font: "bold 32px Arial", fill:"#fff"};
 	// scorelabel = game.add.text(300,560, "Score:", style);
