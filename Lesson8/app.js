39ed700e0bafc0153e77bb71610af6bd06e2797c
lucasyan420 <lucasyan2005@gmail.com>
2019-03-16 15:59:46 +0800

    first push

--- Lesson8/app.js
+++ Lesson8/app.js
@@ -161,7 +161,8 @@ function create(){
 	enemy3.body.velocity.x = +80;
 	enemy3.animations.play('right');
 	enemy4.body.velocity.x = +80;
-	enemy5.body.velocity.x = +80;
+	enemy5.body.velocity.x = -20;
+	enemy5.animations.play('left');
 
 
 }
